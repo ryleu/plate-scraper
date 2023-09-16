9b85d8342bfff2b970b942e0948d110cc8acca09
ryleu <69326171+ryleu@users.noreply.github.com>
2023-09-16 00:44:53 -0500

    please kill me. they have me locked in here

--- main.py
+++ main.py
@@ -14,17 +14,11 @@ else:
         "menu_id": os.environ["MENU_ID"],
         "location_id": os.environ["LOCATION_ID"],
         "where_am_i": os.environ["WHERE_AM_I"],
-        "bot_token": os.environ["BOT_TOKEN"]
+        "bot_token": os.environ["BOT_TOKEN"],
     }
 
 
-def get_menu_data():
-    today = datetime.datetime.now()
-    # mya why did you let me do this
-    config[
-        "date"
-    ] = f"{('0' + str(today.month))[-2:]}/{('0' + str(today.day))[-2:]}/{today.year}"
-
+def get_menu_data(query_day: str):
     base_url = "http://menus.sodexomyway.com/BiteMenu/MenuOnly?menuId={menu_id}&locationId={location_id}&whereami={where_am_i}&startDate={date}"
 
     # cache responses
@@ -33,62 +27,61 @@ def get_menu_data():
     except FileExistsError:
         pass
 
-    html = ""
-    file_name = "cached/" + config["date"].replace("/", "_") + ".html"
+    output = {}
+    file_name = "cached/" + query_day.replace("/", "_") + ".json"
     if os.path.exists(file_name):
         # if data for today exists, just use that
         with open(file_name) as file:
-            html = file.read()
+            output = json.loads(file.read())
     else:
         # if no file for today exists, pull the data from sodexo
-        response = requests.get(base_url.format(**config))
+        response = requests.get(base_url.format(**config, date=query_day))
         if not response.ok:
             raise Exception  # TODO: make this error useful later
         html = response.text
         with open(file_name, "w+") as file:
             file.write(html)
 
-    # this is a hack to pull the json data out of the html
-    # TODO: make this less shit
-    data = json.loads(
-        html.split("<div id='nutData' data-schools='False' class='hide'>")[1].split(
-            "</div>"
-        )[0]
-    )
-
-    # grab only the menu for today
-    menu = {}
-    for entry in data:
-        if entry["currentMenu"]:
-            menu = entry
-            break
-
-    # if something goes wrong and we don't have a menu for today, raise an exception
-    if menu == {}:
-        raise Exception
-
-    # clean up the menu data, sorting it first by meal and then by course
-    clean_menu = {}
-    for item in menu["menuItems"]:
-        meal = item["meal"]
-        course = item["course"]
-
-        if meal not in clean_menu:
-            clean_menu[meal] = {}
-        if course not in clean_menu[meal]:
-            clean_menu[meal][course] = []
-
-        clean_menu[meal][course].append(
-            {
-                "name": item["formalName"],
-                "description": item["description"],
-            }
+        # this is a hack to pull the json data out of the html
+        # TODO: make this less shit
+        data = json.loads(
+            html.split("<div id='nutData' data-schools='False' class='hide'>")[1].split(
+                "</div>"
+            )[0]
         )
 
-    return clean_menu
+        for menu in data:
+            # if something goes wrong and we don't have a menu for today, raise an exception
+            if menu == {}:
+                raise Exception
 
+            # clean up the menu data, sorting it first by meal and then by course
+            clean_menu = {}
+            for item in menu["menuItems"]:
+                meal = item["meal"]
+                course = item["course"]
 
-cached_menus = {}
+                if meal not in clean_menu:
+                    clean_menu[meal] = {}
+                if course not in clean_menu[meal]:
+                    clean_menu[meal][course] = []
+
+                clean_menu[meal][course].append(
+                    {
+                        "name": item["formalName"],
+                        "description": item["description"],
+                    }
+                )
+            year, month, day = menu["date"].split("T")[0].split("-")
+            date = f"{month}/{day}/{year}"
+            if date == query_day:
+                output = clean_menu
+
+            with open(f"cached/{date.replace('/', '_')}.json", "w+") as file:
+                file.write(json.dumps(clean_menu))
+
+    return output
+
 
 bot = interactions.Client()
 
@@ -98,14 +91,28 @@ async def on_startup():
     print("Bot is ready!")
 
 
-@interactions.slash_command(name="menu", description="Get today's menu")
-async def menu(ctx: interactions.SlashContext):
+@interactions.slash_command(
+    name="menu",
+    description="Get today's menu",
+    options=[
+        interactions.SlashCommandOption(
+            name="date",
+            type=interactions.OptionType.STRING,
+            description="Date to get the menu for in the format `MM/DD/YYYY`.",
+            required=False,
+        )
+    ],
+)
+async def menu(ctx: interactions.SlashContext, date: str = ""):
     await ctx.defer()
 
-    data = get_menu_data()
-    cached_menus[config["date"]] = data
+    if date == "":
+        today = datetime.datetime.now()
+        date = f"{('0' + str(today.month))[-2:]}/{('0' + str(today.day))[-2:]}/{today.year}"
+
+    data = get_menu_data(date)
     embed = interactions.Embed(
-        f"Menu for {config['date']}",
+        f"Menu for {date}",
         description="Press a button to get started!",
         color="#184ed7",
     )
@@ -115,7 +122,7 @@ async def menu(ctx: interactions.SlashContext):
             interactions.Button(
                 style=interactions.ButtonStyle.PRIMARY,
                 label=x.lower(),
-                custom_id=f"{config['date']}.{x}",
+                custom_id=f"{date}.{x}",
             )
             for x in data
         ]
@@ -131,16 +138,14 @@ async def button_pressed(event: interactions.events.ButtonPressed):
     custom_id = ctx.custom_id.split(".")
     date = custom_id[0]
     meal_name = custom_id[1]
+    menu = get_menu_data(date)
 
-    meal = cached_menus[date][meal_name]
-
-    if date not in cached_menus:
-        return await ctx.send("This menu is no longer available!", ephemeral=True)
+    meal = menu[meal_name]
 
     if len(custom_id) == 2:
         # this branch runs when a meal button is pressed
         embed = interactions.Embed(
-            f"Entrees for {config['date']}'s {meal_name.lower()}",
+            f"Entrees for {date}'s {meal_name.lower()}",
             description="Press a button to get your menu!",
             color="#184ed7",
         )
@@ -150,7 +155,7 @@ async def button_pressed(event: interactions.events.ButtonPressed):
                 interactions.Button(
                     style=interactions.ButtonStyle.PRIMARY,
                     label=x.lower(),
-                    custom_id=f"{config['date']}.{meal_name}.{x}",
+                    custom_id=f"{date}.{meal_name}.{x}",
                 )
                 for x in meal
             ]
@@ -162,7 +167,7 @@ async def button_pressed(event: interactions.events.ButtonPressed):
         course = meal[course_name]
 
         embed = interactions.Embed(
-            f"{course_name.lower()} menu for {config['date']}'s {meal_name.lower()}",
+            f"{course_name.lower()} menu for {date}'s {meal_name.lower()}",
             color="#184ed7",
         )
 
@@ -177,4 +182,9 @@ async def button_pressed(event: interactions.events.ButtonPressed):
         await ctx.send(embed=embed, ephemeral=True)
 
 
-bot.start(config["bot_token"])
+def main():
+    bot.start(config["bot_token"])
+
+
+if __name__ == "__main__":
+    main()
